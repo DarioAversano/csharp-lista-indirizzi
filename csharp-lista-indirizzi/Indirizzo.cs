6dabaf13fc921c67d6e7c63fc2f38a9bf2a559e6
darioaversano <risusveloce@gmail.com>
2023-04-26 16:41:45 +0200

    Committ

--- csharp-lista-indirizzi/Indirizzo.cs
+++ csharp-lista-indirizzi/Indirizzo.cs
@@ -1,12 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace csharp_lista_indirizzi
 {
-    internal class Indirizzo
+    public class Indirizzo
     {
+        // ATTRIBUTI / CARATTESTICHE
+        private string name;
+        private string surname;
+        private string street;
+        private string city;
+        private string province;
+        private int zip;
+
+        // STATI
+
+        // COSTRUTTORE
+        public Indirizzo(string name, string surname, string street, string city, string province, int zip)
+        {
+
+            if ((name == "") || (surname == "") || (street == ""))
+            {
+                throw new Exception("Inserisci un input");
+            }
+
+            this.name = name;
+            this.surname = surname;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.zip = zip;
+        }
+
+        // GETTERS
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public string GetSurname()
+        {
+            return this.surname;
+        }
+
+        public string GetStreet()
+        {
+            return this.street;
+        }
+
+        public string GetCity()
+        {
+            return this.city;
+        }
+
+        public string GetProvince()
+        {
+            return this.province;
+        }
+
+        public int GetZip()
+        {
+            return this.zip;
+        }
+
+        // SETTERS
+
+        // METODI
+
+
+
+
+        // Metodo ToString() per la stampa dell'indirizzo
+        public override string ToString()
+        {
+            string stringToWrite;
+            stringToWrite = "----------INDIRIZZO----------- \n";
+            stringToWrite += $"\t Name: {this.name} \n";
+            stringToWrite += $"\t Surname: {this.surname} \n";
+            stringToWrite += $"\t Street: {this.street} \n";
+            stringToWrite += $"\t City: {this.city} \n";
+            stringToWrite += $"\t Province: {this.province} \n";
+            stringToWrite += $"\t Zip: {this.zip} \n";
+            stringToWrite += "-------------------------------------------";
+
+            return stringToWrite;
+
+
+
+        }
     }
 }
+
+
--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -1,5 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System;
 
-Console.WriteLine("Hello, World!");
+using csharp_lista_indirizzi;
+List<Indirizzo> listaIndirizzi = new List<Indirizzo>();
 
+try
+{
+    StreamReader file = File.OpenText("C:\\Users\\minec\\Desktop\\Visual Studio 2022 Esercizi\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\addresses.csv");
+    int contatoreRigheLette = 0;
+    while (!file.EndOfStream)
+    {
+
+        string rigaDiTestoLetta = file.ReadLine();
+        contatoreRigheLette++;
+
+        if (contatoreRigheLette > 1)
+        {
+            string[] informazioniSeparate = rigaDiTestoLetta.Split(',');
+
+            if (informazioniSeparate.Length != 6)
+            {
+                Console.WriteLine("La riga " + contatoreRigheLette + "non rispetta lo standard richiesto di 5 informazioni per riga");
+            }
+            else
+            {
+
+                int zip = int.Parse(informazioniSeparate[5]);
+
+                try
+                {
+                    Indirizzo indirizzoLetto = new Indirizzo(informazioniSeparate[0], informazioniSeparate[1], informazioniSeparate[2], informazioniSeparate[3], informazioniSeparate[4], zip);
+                    listaIndirizzi.Add(indirizzoLetto);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine("C'è stato un errore nella lettura dell'indirizzo in riga n° " + contatoreRigheLette);
+                    Console.WriteLine("L'errore è: " + ex.Message);
+                }
+            }
+        }
+    }
+
+}
+catch (Exception e)
+{
+    Console.WriteLine("Qualcosa è andato storto...");
+    Console.WriteLine(e.Message);
+}
+
+
+
+// Stampa indirizzo dalla lista
+foreach (Indirizzo indirizzo in listaIndirizzi)
+{
+    Console.WriteLine(indirizzo);
+}
